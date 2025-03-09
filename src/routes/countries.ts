3388f24e0d4187db361f6afe9484216bd3fec988
zblednu <maksym.surovtsev@student.tuke.sk>
2025-03-09 14:38:01 +0100

    borders and population, move logic to async functions

--- src/routes/countries.ts
+++ src/routes/countries.ts
@@ -4,12 +4,12 @@ import { error } from "console";
 
 const router = Router();
 
-if (!process.env.EXTERNAL_API) throw new Error("EXTERNAL_API is not set");
+if (!process.env.EXTERNAL_API_1) throw new Error("EXTERNAL_API_1 is not set");
 
 
 router.route('/')
 	.get((_, res) => {
-		axios.get(`${process.env.EXTERNAL_API}/AvailableCountries`)
+		axios.get(`${process.env.EXTERNAL_API_1}/AvailableCountries`)
 			.then(response => {
 				const countries = response.data.map(
 					(elem: { name: string, countryCode: string }) => elem.name);
@@ -23,22 +23,49 @@ router.route('/')
 router.route('/:country')
 	.get((req, res) => {
 		const promises = [
-			axios.get(`${process.env.EXTERNAL_API}/CountryInfo/${req.params.country}`),
-			//axios.get(`${process.env.EXTERNAL_API!}/countries/population`),
-			//axios.get(`${process.env.EXTERNAL_API!}/countries/flag/images`)
+			fetchBorders(req.params.country),
+			fetchPopulation(req.params.country)
 		]
 
 		Promise.all(promises)
 			.then(values => {
-				const data = values.map(elem => elem.data);
-				const borderCountries = (data[0].borders as Array<any>)
-					.map((elem: { commonName: string }) => elem.commonName);
 
-				res.send(borderCountries);
+				res.json({
+					borders: values[0],
+					population: values[1],
+				});
 			})
 			.catch(error => {
 				res.status(500).send(error);
 			});
 	});
+
+
+async function fetchBorders(country: string) {
+	const allRoute = `${process.env.EXTERNAL_API_1}/AvailableCountries`
+	const code = await axios.get(allRoute)
+		.then(res => {
+			return res.data
+				.find((elem: { countryCode: string, name: string }) => elem.name === country)
+				.countryCode;
+		});
+
+	const specRoute = `${process.env.EXTERNAL_API_1}/CountryInfo/${code}`
+	return axios.get(specRoute)
+		.then(res => {
+			return res.data.borders as Array<any>
+		});
+}
+
+async function fetchPopulation(country: string) {
+	const route = `${process.env.EXTERNAL_API_2}/countries/population`;
+
+	return axios.get(route)
+		.then(res => {
+			return res.data.data.find((elem: { country: string }) => elem.country === country).populationCounts as Array<any>
+		})
+}
+
+//async function 
 
 export default router;
